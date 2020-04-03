e72cc3563afcd1a8e4030389703ed67a07140587
ryan <ryansemple91@gmail.com>
2020-04-02 18:05:04 -0700

    More improvements to comments

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,10 +1,18 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * Used to keep track of previous values of a state property.
+ * 
+ * @param value - the state property that you want to keep track 
+ * of previous versions of.
+ */
 const usePrevious = <T extends {}>(value: T): T | undefined => {
-  const ref = useRef<T>();
+	const ref: React.MutableRefObject<T | undefined> = useRef<T>();
+	
   useEffect(() => {
     ref.current = value;
-  });
+	});
+	
   return ref.current;
 };
 
